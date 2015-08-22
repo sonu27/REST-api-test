82f763073589bda50b8e69b85b3a7c3bc8dc8694
Amarjeet Singh Rai <jatt27@gmail.com>
2015-08-22 18:09:58 +0100

    Implement stateless api auth

--- src/AppBundle/Entity/UserRepository.php
+++ src/AppBundle/Entity/UserRepository.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Doctrine\ORM\Query;
 
 /**
  * UserRepository
@@ -12,4 +15,30 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
+
+    public function findByApiKey($key)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('u')
+            ->from('AppBundle:User', 'u')
+            ->where('u.apiKey = :key')
+            ->setParameter('key', $key);
+        $query = $qb->getQuery();
+
+        return $this->getSingleResult($query);
+    }
+
+    private function getSingleResult(Query $query)
+    {
+        try {
+            $result = $query->getSingleResult();
+        } catch (NonUniqueResultException $e) {
+            $result = null;
+        } catch (NoResultException $e) {
+            $result = null;
+        }
+
+        return $result;
+    }
+
 }
