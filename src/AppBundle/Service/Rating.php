8a373619b5146ccfd0a72699c9f19329f9548fc3
Amarjeet Singh Rai <jatt27@gmail.com>
2015-08-25 22:53:58 +0100

    Add rating service

---
+++ src/AppBundle/Service/Rating.php
@@ -0,0 +1,50 @@
+<?php
+namespace AppBundle\Service;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class Rating
+{
+    private $userRepo;
+    private $commentRepo;
+
+    public function __construct($userRepo, $commentRepo)
+    {
+        $this->userRepo    = $userRepo;
+        $this->commentRepo = $commentRepo;
+    }
+
+    public function addRating(Request $request, \AppBundle\Entity\Comment $comment)
+    {
+        if ($this->validateRequest($request)) {
+            $userId   = $request->request->get('user');
+            $thumbsUp = $request->request->get('rating');
+            $user     = $this->userRepo->findById($userId);
+
+            $comment->addRating($this->createRating($user, $thumbsUp));
+            $this->commentRepo->save($comment);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function validateRequest(Request $request)
+    {
+        if ($request->request->has('user') && $request->request->has('rating')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function createRating($user, $thumbsUp)
+    {
+        $rating = new \AppBundle\Entity\Rating();
+        $rating->setUser($user);
+        $rating->setGood($thumbsUp);
+
+        return $rating;
+    }
+}
